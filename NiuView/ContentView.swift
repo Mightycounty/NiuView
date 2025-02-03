bab4fb64cb06b688a29fc41aff46e5fc5e870da3
Mightypants <lbwanghr@icloud.com>
2025-02-03 19:27:30 +0800

    Initial Commit

---
+++ NiuView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NiuView
+//
+//  Created by Haoran Wang on 2025/2/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NiuView/NiuViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiuViewApp.swift
+//  NiuView
+//
+//  Created by Haoran Wang on 2025/2/3.
+//
+
+import SwiftUI
+
+@main
+struct NiuViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
